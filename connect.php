354f4e917470b34104682d933636f0edb1e46e3f
Nishan Ahmed <nishanahmad@gmail.com>
2018-03-02 10:00:04 +0530

    ignore fle

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php	
-	$con=mysqli_connect("localhost","nishan","darussalam123.","nas");
-	// Check connection
-	if ($con->connect_error) 
-	{
-		die("Connection failed: " . $con->connect_error);
-	}
-?>
