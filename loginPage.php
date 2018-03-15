75842fe17cd5c3b8abad13dfb11fd67a9ff3301d
Nishan Ahmed <nishanahmad@gmail.com>
2018-03-15 11:28:22 +0530

    15 March, 2018

--- loginPage.php
+++ loginPage.php
@@ -1,38 +1,50 @@
+
 <html>
-<head>
-<div align="center">
-<br><br><br>
-<img src="images/logoBlack.png"  width='300px' height='50px'>
-</div>
-
-<body>
 <title>User Login</title>
-<link rel="stylesheet" type="text/css" href="css/loginPage.css" />
+<head>
+<link rel="stylesheet" type="text/css" href="css/login.css">
+<script src="js/jquery3.min.js"></script>
+<script type="text/javascript">
+$("#login-button").click(function(event){
+	event.preventDefault();	 
+	$('form').fadeOut(500);
+	$('.wrapper').addClass('form-success');
+});
+</script>
 </head>
-
-<form class="form-4" method="post" action="user_login_session.php">
-    <div align="center">
-	<h1>USER LOGIN </h1>
+<body>
+	<div align="center">
+		<br/><br/>
+		<img src="images/logo.png"  width='400px' height='70px'/>
+		<div class="wrapper">
+			<div class="container">																											<?php 
+				if(isset($_GET['message']))
+				{
+					echo '<font style="color:red;text-shadow: none;font-weight:bold;font-size:20px;">'.$_GET['message'].'</font>';
+				}						
+				else
+					echo '<h1>Welcome</h1>';																								?>						
+				<form class="form" name="frmUser" method="post" action="user_login_session.php">
+					<input type="text" name="user_name" placeholder="Username" required>
+					<input type="password" name='password' placeholder="Password" required> 			
+					<button type="submit" id="login-button">Login</button>
+				</form>
+			</div>
+			
+			<ul class="bg-bubbles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
 	</div>
-<?php 
-	if(isset($_GET['message']))
-	{
-		echo '<font style="color:#D20101;text-shadow: none;font-weight:bold;font-size:20px;">'.$_GET['message'].'</font>';
-	}
-?>		
-    <p>
-        <label for="login">Username</label>
-        <input type="text" name="user_name" placeholder="Username" required>
-    </p>
-    <p>
-        <label for="password">Password</label>
-        <input type="password" name='password' placeholder="Password" required> 
-    </p>
-
-    <p>
-        <input type="submit" name="submit" value="Continue"
-    </p>       
-</form>
 </body>
 </html>
 
